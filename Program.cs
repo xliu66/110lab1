b17f1f20857b3fbdfb1cfdb48f554e3d53d61c0b
xliu66 <56804761+xliu66@users.noreply.github.com>
2020-03-09 14:17:15 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Test();
+        }
+        static void Test()
+        {
+            Console.WriteLine("This is a method");
+
+            /* Variables and Data types */
+            string firstName = "Xiaochen";
+            Console.WriteLine("My name is: " + firstName); 
+
+            int age = 99;
+            long seconds = 128749475969789;
+
+            float price = 12.34f;
+            decimal money = 123.23m;
+            
+            bool isThisCrazy = true;
+
+            // if statements
+            if(isThisCrazy)
+            {
+                Console.WriteLine("Yeah it is");
+            }
+            else
+            {
+                Console.WriteLine("No, it is not");
+            }
+
+            // loops 
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine("Iteration: "+i);
+            }
+        }
+    }
+}
